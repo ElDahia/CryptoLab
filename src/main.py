7838c5dd7e459eab697c435df35a6e5b0f4622f7
ElDahia <apooragp00@gmail.com>
2026-09-23 14:57:10 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,10 @@
-print("CryptoLab started")
+from market.market_data import get_price
+
+
+def main():
+    btc_price = get_price("BTCUSDT")
+    print(f"BTC/USDT: {btc_price}")
+
+
+if __name__ == "__main__":
+    main()
